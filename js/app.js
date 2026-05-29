fb51523930782a04dc9af9708e612372d3282c8f
TipsyRoaming <TipsyRoamingInTheWorld@gmail.com>
2026-05-29 20:27:43 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3,13 +3,11 @@ import { initSearch } from "./search.js";
 import { initWeather } from "./weather.js";
 import { initUI } from "./ui.js";
 import { initReview } from "./review.js";
-import { initAuth } from "./auth.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     initI18n();
     initSearch();
     initWeather();
     initUI();
-    initAuth();
     initReview();
 });
